ace57b59b08ee5df1542b4a0c3e9661256640da9
Kamil Nowak <kamil.nowak@kotrak.com>
2019-04-25 13:26:40 +0200

    activity indicator

--- XamarinTSP/XamarinTSP/UI/ViewModels/MainViewModel.cs
+++ XamarinTSP/XamarinTSP/UI/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
@@ -18,7 +19,7 @@ namespace XamarinTSP.UI.ViewModels
         private TSPConfiguration _tspConfiguration;
         private INavigator _navigator;
         private IGeolocationService _geolocation;
-
+        public bool IsTSPRunning { get; private set; }
         public CustomMapController MapController { get; private set; }
         public LocationList List { get; private set; }
 
@@ -34,15 +35,12 @@ namespace XamarinTSP.UI.ViewModels
             _tspAlgorithm = new TSPAlgorithm(_tspConfiguration);
         }
 
-
         public ICommand OnAppearingCommand => new Command(() =>
         {
             if (List.Locations.Count == 0)
             {
                 List.SetMockData(_geolocation);
             }
-            //MapController.CustomMap.UpdatePins();
-            //MapController.CustomMap.FocusPins();
         });
         public ICommand SelectCommand => new Command<Location>(async selected =>
         {
@@ -63,44 +61,66 @@ namespace XamarinTSP.UI.ViewModels
         });
         public ICommand RunTSPCommand => new Command<Button>(async button =>
         {
-            var dest = List.Locations.Select(x => $"{x.Position.Latitude},{x.Position.Longitude}").ToArray();
-            var configuration = new DistanceMatrixRequestConfiguration()
-            {
-                Origins = dest,
-                Destinations = dest
-            };
-            int[] result = null;
-            try
-            {
-                var response = await _googleMapsService.GetDistanceMatrix(configuration);
-                var data = new DistanceMatrixData(response);
-
-                var tsp = _tspAlgorithm.Run(new TSPData(data.DistanceMatrix, data.DurationMatrix, _tspConfiguration.ReturnToOrigin), List.Locations.Count * 200);
-
-                result = tsp.waypoints;
-
-                var route = new List<Location>();
-                for (int i = 0; i < result.Length; i++)
-                {
-                    route.Add(List.Locations.ElementAt(result[i]));
-                }
-                if (_tspConfiguration.ReturnToOrigin)
-                    route.Add(List.Locations.ElementAt(0));
-
-                MapController.DisplayRoute(route.Select(x => x.Position).ToList());
-                MapController.CalculatedRoute.Distance = Distance.FromMeters(Math.Round(tsp.distance / 1000));
-                MapController.CalculatedRoute.Time = TimeSpan.FromSeconds(tsp.time);
-
-                NotifyOfPropertyChange(() => MapController.CalculatedRoute.Time);
-                NotifyOfPropertyChange(() => MapController.CalculatedRoute.Distance);
-            }
-            catch (Exception ex)
-            {
-
-                await Application.Current.MainPage.DisplayAlert("TSP ERROR", ex.Message, "OK");
+            if (IsTSPRunning)
                 return;
-            }
-            await Application.Current.MainPage.DisplayAlert("TSP FINISHED", "Optimal route has been calculated", "OK");
+            IsTSPRunning = true;
+            NotifyOfPropertyChange(() => IsTSPRunning);
+            button.Image = new FileImageSource() { File = button.Image.File.Replace("black", "blue_light") };
+
+            await Task.Run(async () =>
+            {
+                var dest = List.Locations.Select(x => $"{x.Position.Latitude},{x.Position.Longitude}").ToArray();
+                var configuration = new DistanceMatrixRequestConfiguration()
+                {
+                    Origins = dest,
+                    Destinations = dest
+                };
+                int[] result = null;
+                try
+                {
+                    var response = await _googleMapsService.GetDistanceMatrix(configuration);
+                    var data = new DistanceMatrixData(response);
+
+                    var tsp = _tspAlgorithm.Run(new TSPData(data.DistanceMatrix, data.DurationMatrix, _tspConfiguration.ReturnToOrigin), List.Locations.Count * 200);
+
+                    result = tsp.waypoints;
+
+                    var route = new List<Location>();
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        route.Add(List.Locations.ElementAt(result[i]));
+                    }
+                    if (_tspConfiguration.ReturnToOrigin)
+                        route.Add(List.Locations.ElementAt(0));
+                    await Helper.InvokeOnMainThreadAsync(() =>
+                    {
+                        MapController.DisplayRoute(route.Select(x => x.Position).ToList());
+                        MapController.CalculatedRoute.Distance = Distance.FromMeters(Math.Round(tsp.distance / 10));
+                        MapController.CalculatedRoute.Time = TimeSpan.FromSeconds(tsp.time);
+
+                        NotifyOfPropertyChange(() => MapController.CalculatedRoute.Time);
+                        NotifyOfPropertyChange(() => MapController.CalculatedRoute.Distance);
+                    });
+
+                }
+                catch (Exception ex)
+                {
+                    await Helper.InvokeOnMainThreadAsync(async () => await Application.Current.MainPage.DisplayAlert("TSP ERROR", ex.Message, "OK"));
+                    return;
+                }
+                finally
+                {
+                    await Helper.InvokeOnMainThreadAsync(() =>
+                    {
+                        IsTSPRunning = false;
+                        button.Image = new FileImageSource() { File = button.Image.File.Replace("blue_light", "black") };
+                        NotifyOfPropertyChange(() => IsTSPRunning);
+                    });
+
+                }
+                //await Helper.InvokeOnMainThreadAsync(async () => await Application.Current.MainPage.DisplayAlert("TSP FINISHED", "Optimal route has been calculated", "OK"));
+            }).ConfigureAwait(false);
+
         });
     }
 }
